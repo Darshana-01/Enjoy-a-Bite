eb1a94ea7dedad747d147112a40e48b25b541a8e
Darshana-01 <88197880+Darshana-01@users.noreply.github.com>
2022-03-05 11:33:42 +0530

    Initial commit

---
+++ enjoy-a-bite/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Restaurant from "./Component/Basics/Restaurant";
+// import UseState from './Component/Hooks/useState';
+
+const App = () => {
+  return (
+    <Restaurant />
+  )
+}
+
+export default App
